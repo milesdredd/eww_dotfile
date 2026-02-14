70e0edd19a6d614ffae2abd7844caa817eb3780c
milesdredd <ankitraj97093@gmail.com>
2026-02-15 03:32:53 +0530

    stable update

--- modules/blue.sh
+++ modules/blue.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
+
 mac=$(bluetoothctl devices Connected | awk '{print $2}' | head -n 1)
+
 if [ -z "$mac" ]; then
     echo '{ "connected": "false", "battery": "null" }'
 else
-    mac_underscore=$(echo "$mac" | tr ':' '_')
-    sink=$(pactl list sinks short | grep -i "$mac_underscore")
-    if [ -z "$sink" ]; then
-        echo '{ "connected": "false", "battery": "null" }'
+    raw=$(bluetoothctl info "$mac" | awk -F'[: ]+' '/Battery/ {print $3; exit}')
+
+    if [[ "$raw" =~ ^0x ]]; then
+        battery=$((raw))
     else
-        battery=$(bluetoothctl info "$mac" | awk '/Battery:/ {print $2}')
-        if [ -z "$battery" ]; then
-            battery=$(upower -e | grep -i "$mac_underscore" | xargs -I {} upower -i {} 2>/dev/null | awk '/percentage:/ {print $2}' | tr -d '%')
-        fi
-        [ -z "$battery" ] && battery="null"
-        echo "{ \"connected\" : \"true\", \"battery\" : \"$battery\" }"
-        
+        battery=$(echo "$raw" | tr -d '%')
     fi
+
+    [ -z "$battery" ] && battery="null"
+
+    echo "{ \"connected\": \"true\", \"battery\": \"$battery\" }"
 fi
+
